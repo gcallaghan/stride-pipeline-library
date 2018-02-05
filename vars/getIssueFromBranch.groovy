c9bca8abf1637fd275d9379e1fafce7c32cac52c
Benjamin Stein <ben@liatrio.com>
2018-02-05 16:14:05 -0500

    removing quotes from env reference

--- vars/getIssueFromBranch.groovy
+++ vars/getIssueFromBranch.groovy
@@ -1,5 +1,5 @@
 def call() {
-    def jiraIssue = ( ${env.branch} +~ /\s*([A-Z]+-[0-9]+)/ )
+    def jiraIssue = ( env.branch +~ /\s*([A-Z]+-[0-9]+)/ )
     if (jiraIssue?.trim())
         return jiraIssue
     throw Exception("Branch does not contain a reference to a ticket")
