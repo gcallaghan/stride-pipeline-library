f7c1b23817a2d397d75d58d4fb527124fcdd40ea
Benjamin Stein <ben@liatrio.com>
2018-02-05 16:44:57 -0500

    use array to grab issue

--- vars/getIssueFromBranch.groovy
+++ vars/getIssueFromBranch.groovy
@@ -1,11 +1,16 @@
 def call() {
     echo "${env.BRANCH_NAME}"
-    def jiraIssue = ( "${env.BRANCH_NAME}" =~ /\s*([A-Z]+-[0-9]+)/ )
-    echo "Jira isssue is ${jiraIssue}"
-    env.JIRA_ISSUE = jiraIssue
-    echo ""
+    try {
+        def jiraIssue = ( "${env.BRANCH_NAME}" =~ /\s*([A-Z]+-[0-9]+)/ )[0][0]
+        echo "Jira isssue is ${jiraIssue}"
+        env.JIRA_ISSUE = jiraIssue
+        echo ""
+    }
+    catch {
+        throw Exception("Branch does not contain a reference to a ticket")
+    }
     if (jiraIssue?.trim())
         return jiraIssue
-    throw Exception("Branch does not contain a reference to a ticket")
+
 
 }
