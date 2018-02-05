253e175d6fb8521642ed501a144e0aee65047e1b
Benjamin Stein <ben@liatrio.com>
2018-02-05 16:57:08 -0500

    debug

--- vars/getIssueFromBranch.groovy
+++ vars/getIssueFromBranch.groovy
@@ -1,5 +1,5 @@
 def call() {
-    echo "${env.BRANCH_NAME}"
+    echo "printing out branch name ${env.BRANCH_NAME}"
     try {
         def jiraIssue = ( "${env.BRANCH_NAME}" =~ /\s*([A-Z]+-[0-9]+)/ )[0][0]
         echo "Jira isssue is ${jiraIssue}"
@@ -11,6 +11,5 @@ def call() {
     }
     if (jiraIssue?.trim())
         return jiraIssue
-
 
 }
