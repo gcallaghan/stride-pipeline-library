666077871ce0973afa3e32902b212346337cd15d
Benjamin Stein <ben@liatrio.com>
2018-02-05 15:43:37 -0500

    add method to get jira issue from Branch

---
+++ vars/getIssueFromBranch.groovy
@@ -0,0 +1,7 @@
+def call() {
+    def jiraIssue = ( ${env.branch} +~ /\s*([A-Z]+-[0-9]+)/
+    if (jiraIssue?.trim())
+        return jiraIssue
+    throw Exception("Branch does not contain a reference to a ticket")
+
+}
