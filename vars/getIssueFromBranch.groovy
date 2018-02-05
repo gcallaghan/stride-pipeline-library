b8e4b16810408277c29a888fab81a2cb600973b0
Benjamin Stein <ben@liatrio.com>
2018-02-05 16:38:14 -0500

    print out jira issue

--- vars/getIssueFromBranch.groovy
+++ vars/getIssueFromBranch.groovy
@@ -1,6 +1,8 @@
 def call() {
     def jiraIssue = ( "${env.branch}" +~ /\s*([A-Z]+-[0-9]+)/ )
-    "${env.JIRA_ISSUE}" = jiraIssue
+    echo "Jira isssue is ${jiraIssue}"
+    JIRA_ISSUE = jiraIssue
+    echo ""
     if (jiraIssue?.trim())
         return jiraIssue
     throw Exception("Branch does not contain a reference to a ticket")
