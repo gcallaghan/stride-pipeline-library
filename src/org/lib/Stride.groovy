0d2f75f8ac60847ee40265fb2552cca52e6894f8
Benjamin Stein <ben@liatrio.com>
2018-02-22 11:11:52 -0500

    groovy grape setup

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -1,6 +1,9 @@
 package org.lib;
 
 @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.1')
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+
 
 class Stride {
 
