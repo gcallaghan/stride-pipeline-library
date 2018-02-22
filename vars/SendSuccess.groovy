d96a74e10cab45180f7fec15d1b09ff31864a9bd
Benjamin Stein <ben@liatrio.com>
2018-02-22 11:07:10 -0500

    println

--- vars/SendSuccess.groovy
+++ vars/SendSuccess.groovy
@@ -3,8 +3,8 @@
 
 def call() {
     println "success"
-    def stride = new org.lib.Stride()
-    stride.sendSuccess()
+//    def stride = new org.lib.Stride()
+//    stride.sendSuccess()
 //    RESTClient client = new RESTClient(clientHostName)
 //    String plainTextToken = "PasswordIsAuthToken:" + authToken
 //    def authorizationToken = plainTextToken.bytes.encodeBase64()
