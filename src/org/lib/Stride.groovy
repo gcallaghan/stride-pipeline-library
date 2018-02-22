d05d3b322478961b528599c17a5f6e8feb013e61
Benjamin Stein <ben@liatrio.com>
2018-02-22 17:37:47 -0500

    trying textbody object

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -8,11 +8,28 @@ import static groovyx.net.http.ContentType.*
 class Stride {
     //site/{cloudId}/conversation/{conversationId}/message
     String sendSuccess(clientHostName, fullPath, authToken) {
+
+        def textBody = """
+                         {
+              "type": "text",
+              "text": "Hello world",
+              "marks": [
+                {
+                  "type": "textColor",
+                  "attrs": {
+                    "color": "#97a0af"
+                  }
+                }
+              ]
+            }
+        """
+
+
         RESTClient client = new RESTClient(clientHostName)
         client.headers['Authorization'] = authToken
         client.ignoreSSLIssues()
         def body = "<h1 style='font-color: red;'>THIS IS RED</h1>"
-        def response = client.post(path: fullPath, requestContentType: TEXT, body: body)
+        def response = client.post(path: fullPath, requestContentType: TEXT, body: textBody)
         println response.data
         return response.data['cloudId']
     }
