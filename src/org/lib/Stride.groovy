1f79773e67d4b612f1a6891efaa72583b089f214
Benjamin Stein <ben@liatrio.com>
2018-02-22 17:17:22 -0500

    test post message

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -6,12 +6,12 @@ import static groovyx.net.http.ContentType.*
 
 
 class Stride {
-
+    //site/{cloudId}/conversation/{conversationId}/message
     String sendSuccess(clientHostName, fullPath, authToken) {
         RESTClient client = new RESTClient(clientHostName)
         client.headers['Authorization'] = authToken
         client.ignoreSSLIssues()
-        def response = client.get(path: fullPath)
+        def response = client.post(path: fullPath, body: "Building Code from: ")
         println response.data
         return response.data['cloudId']
     }
