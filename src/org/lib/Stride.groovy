5a3079d87732b88783c3fb28c590178a719d555f
Benjamin Stein <ben@liatrio.com>
2018-02-23 09:47:21 -0500

    fixed formatting of JSON object

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -6,57 +6,36 @@ import static groovyx.net.http.ContentType.*
 import groovy.json.JsonSlurper
 
 class Stride {
-    //site/{cloudId}/conversation/{conversationId}/message
     String sendSuccess(clientHostName, fullPath, authToken) {
 
-        def textBody = """
-                        {
-  "version": 1,
-  "type": "doc",
-  "content": [
-    {
-      "type": "panel",
-      "attrs": {
-        "panelType": "warning"
-      },
-      "content": [
-        {
-          "type": "paragraph",
-          "content": [
-            {
-              "type": "text",
-              "text": "Build Fail"
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-        """
         def requestBody = [
                 version: 1,
                 type   : "doc",
                 content: [
                         [
                                 type   : "panel",
-                                attrs  : [
-                                        panelType: warning
+                                attrs  : [panelType: "warning"
                                 ],
                                 content: [
-                                        type: "text",
-                                        text: "Build Fail"
+                                        [
+                                                type   : "paragraph",
+                                                content: [
+                                                        [
+                                                                type: "text",
+                                                                text: "Build Fail"
+                                                        ]
+                                                ]
+                                        ]
+
                                 ]
                         ],
 
 
                 ]
-
         ]
         RESTClient client = new RESTClient(clientHostName)
         client.headers['Authorization'] = authToken
         client.ignoreSSLIssues()
-        def body = "<h1 style='font-color: red;'>THIS IS RED</h1>"
         def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
         println response.data
         return response.data['cloudId']
