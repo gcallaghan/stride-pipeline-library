36f3f58983b0c9f4ba1e8dc3a8462fadf9053187
Benjamin Stein <ben@liatrio.com>
2018-02-23 10:11:47 -0500

    add methods for success, failure, and info

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -3,43 +3,65 @@ package org.lib;
 @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.1')
 import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.*
-import groovy.json.JsonSlurper
 
 class Stride {
+    def paneltype = [
+            FAIL : "warning",
+            INFO : "info",
+            SUCCESS :  "tip"
+    ]
+    def requestBody = [
+            version: 1,
+            type   : "doc",
+            content: [
+                    [
+                            type   : "panel",
+                            attrs  : [panelType: null
+                            ],
+                            content: [
+                                    [
+                                            type   : "paragraph",
+                                            content: [
+                                                    [
+                                                            type: "text",
+                                                            text: null
+                                                    ]
+                                            ]
+                                    ]
+
+                            ]
+                    ],
+
+
+            ]
+    ]
     String sendSuccess(clientHostName, fullPath, authToken, message) {
-
-        def requestBody = [
-                version: 1,
-                type   : "doc",
-                content: [
-                        [
-                                type   : "panel",
-                                attrs  : [panelType: "tip"
-                                ],
-                                content: [
-                                        [
-                                                type   : "paragraph",
-                                                content: [
-                                                        [
-                                                                type: "text",
-                                                                text: null
-                                                        ]
-                                                ]
-                                        ]
-
-                                ]
-                        ],
-
-
-                ]
-        ]
         requestBody.content[0].content[0].content[0].text = message
+        requestBody.content[0].attrs.panelType = paneltype.SUCCESS
         RESTClient client = new RESTClient(clientHostName)
         client.headers['Authorization'] = authToken
         client.ignoreSSLIssues()
         def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
         println response.data
         return response.data['cloudId']
+    }
+    String sendFailure() {
+        requestBody.content[0].content[0].content[0].text = message
+        requestBody.content[0].attrs.panelType = paneltype.FAIL
+        RESTClient client = new RESTClient(clientHostName)
+        client.headers['Authorization'] = authToken
+        client.ignoreSSLIssues()
+        def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
+        println response.data
+    }
+    String sendInfo() {
+        requestBody.content[0].content[0].content[0].text = message
+        requestBody.content[0].attrs.panelType = paneltype.INFO
+        RESTClient client = new RESTClient(clientHostName)
+        client.headers['Authorization'] = authToken
+        client.ignoreSSLIssues()
+        def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
+        println response.data
     }
 }
 
