c10ff13be2c9d859bc1e216678d4f45b2cc7c458
Benjamin Stein <ben@liatrio.com>
2018-02-23 11:14:26 -0500

    pass in correct parameters

--- src/org/lib/Stride.groovy
+++ src/org/lib/Stride.groovy
@@ -5,6 +5,7 @@ import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.*
 
 class Stride {
+    String strideHostname = 'https://api.atlassian.com'
     def paneltype = [
             FAIL : "warning",
             INFO : "info",
@@ -33,33 +34,37 @@ class Stride {
                     ],
             ]
     ]
-    String sendSuccess(clientHostName, fullPath, authToken, message) {
+    String sendSuccess(conversationId, message, strideToken, orgId) {
         requestBody.content[0].content[0].content[0].text = message
         requestBody.content[0].attrs.panelType = paneltype.SUCCESS
-        RESTClient client = new RESTClient(clientHostName)
-        client.headers['Authorization'] = authToken
+        RESTClient client = new RESTClient(strideHostname)
+        client.headers['Authorization'] = strideToken
         client.ignoreSSLIssues()
-        def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
+        def response = client.post(path: getConversationPath(orgId,conversationId), requestContentType: "application/json", body: requestBody)
         println response.data
         return response.data['cloudId']
     }
-    String sendFailure(clientHostName, fullPath, authToken, message) {
+    String sendFailure(conversationId, message, strideToken, orgId) {
         requestBody.content[0].content[0].content[0].text = message
         requestBody.content[0].attrs.panelType = paneltype.FAIL
-        RESTClient client = new RESTClient(clientHostName)
-        client.headers['Authorization'] = authToken
+        RESTClient client = new RESTClient(strideHostname)
+        client.headers['Authorization'] = strideToken
         client.ignoreSSLIssues()
-        def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
+        def response = client.post(path: getConversationPath(orgId,conversationId), requestContentType: "application/json", body: requestBody)
         println response.data
     }
-    String sendInfo(clientHostName, fullPath, authToken, message) {
+    String sendInfo(conversationId, message, strideToken, orgId) {
         requestBody.content[0].content[0].content[0].text = message
         requestBody.content[0].attrs.panelType = paneltype.INFO
-        RESTClient client = new RESTClient(clientHostName)
-        client.headers['Authorization'] = authToken
+        RESTClient client = new RESTClient(strideHostname)
+        client.headers['Authorization'] = strideToken
         client.ignoreSSLIssues()
-        def response = client.post(path: fullPath, requestContentType: "application/json", body: requestBody)
+        def response = client.post(path: getConversationPath(orgId,conversationId), requestContentType: "application/json", body: requestBody)
         println response.data
+    }
+
+    def getConversationPath(orgId,conversationId) {
+        return "/site/${orgId}/conversation/${conversationId}"
     }
 }
 
