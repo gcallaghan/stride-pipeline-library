4c06ba3e5edd0b56cbe3fb73a5511a4e6999e30e
Benjamin Stein <ben@liatrio.com>
2018-02-23 14:11:09 -0500

    remove no longer used var

--- vars/SendSuccess.groovy
+++
@@ -1,8 +0,0 @@
-#!/bin/env groovy
-
-
-def call(clientHostName,fullPath,authToken) {
-    println "success"
-    def stride = new org.lib.Stride()
-    println stride.sendSuccess(clientHostName,fullPath,authToken)
-}
